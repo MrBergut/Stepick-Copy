77a5435fee4cfe6e9fe71bfe7ecb1bf9eb2f2fc4
MrBergut <torgovickijdana@gmail.com>
2023-06-07 15:23:43 +0300

    modal window autorization

---
+++ src/components/autorization.jsx
@@ -0,0 +1,11 @@
+import './styles/autorization.css'
+
+export default function Autorization({active, setActive}) {
+    return (
+        <div className='autorization'>
+            <div className='autorization_content'>
+
+            </div>
+        </div>
+    )
+}
