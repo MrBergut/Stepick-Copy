6a1222d2020a216d70239b417d3a6ec085e4829e
shadowasp <shadowasp@yandex.ru>
2023-06-10 22:48:05 +0300

    small refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import Header from './containers/Header';
-import Content from './containers/Content';
-import Footer from './containers/Footer';
-import './styles/markup.css'
-import logo from './assets/images/topbar_logo.svg'
+import Header from "./containers/Header";
+import Content from "./containers/Content";
+import Footer from "./containers/Footer";
 
-
+import "./styles/markup.css";
+import logo from "./assets/images/topbar_logo.svg";
 
 function App() {
-  
   return (
-    <div className='App'>
-      <Header logo={logo}/>
+    <div className="App">
+      <Header logo={logo} />
       <Content />
       <Footer logo={logo} />
-      
     </div>
   );
 }
