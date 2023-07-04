5cf0b7f23630310a759c1bef18da519fbc6072e3
MrBergut <torgovickijdana@gmail.com>
2023-07-04 22:22:15 +0300

    Any changes to Search are committed to the console, and when the button is clicked, the final query is committed.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,36 @@
 import Input from './Input';
 import Button from './Button';
 
+import { useState, useCallback } from 'react';
 
 
 import '../styles/search.css';
 import searchlogo from '../assets/images/icons8search30.png';
 
 export default function Search({}) {
+    const [inputSearch, setInputSearch] = useState('');
     
+    const logInputSearch = useCallback(() => {
+        console.log(`Current request: ${inputSearch}`);
+    }, [inputSearch]);
+    const handleInputChange = (e) => {
+        setInputSearch(e.target.value);
+        logInputSearch();
+    };
+    const confirmSearch = () => {
+        console.log(`request: ${inputSearch}`)
+    };
 
     return (
         <div id='search'>
-            <Input placeholder='поиск' />
-            <form action='./index.html' target='_top'>
-                <Button type='search'>
+            <Input 
+                placeholder='поиск'
+                type='text'
+                value={inputSearch}
+                onChange={handleInputChange} />
+                <Button type='search' onClick={confirmSearch}>
                     <img src={searchlogo} alt='saerchlogo' />
                 </Button>    
-            </form>
         </div>
     );
 }
